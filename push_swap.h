0a5001a9a298ce73cc1fa07b1c7db3c75c5e099b
Abderrahim Jellal <abjellal@c2r8p1.1337.ma>
2025-01-19 10:22:19 +0100

    i added some code

--- push_swap.h
+++ push_swap.h
@@ -1,5 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abjellal <abjellal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 09:33:45 by abjellal          #+#    #+#             */
+/*   Updated: 2025/01/19 10:16:55 by abjellal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #ifndef PUSH_SWAP_H
 #define PUSH_SWAP_H
+
+#include "../libft/libft.h"
 
 typedef struct s_list
 {
@@ -8,6 +23,16 @@ typedef struct s_list
     struct s_list *next;
 } t_list;
 
-
+int pa(t_list **stack_a, t_list **stack_b);
+int pb(t_list **stack_a, t_list **stack_b);
+int ra(t_list **stack_a);
+int rb(t_list ** stack_b);
+int rr(t_list **stack_a, t_list **stack_b);
+int rra(t_list **stack_a);
+int rrb(t_list **stack_b);
+int rrr(t_list **stack_a, t_list ** stack_b);
+int sa(t_list **stack_a);
+int sb(t_list **stack_b);
+int ss(t_list **stack_a, t_list **stack_b)
 
 #endif
